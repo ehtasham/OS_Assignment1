891433e74d0338aad407685c6534dfb20d4d4408
ehtasham <ehtashamawan@gmail.com>
2018-03-10 17:17:16 +0500

    updated server

--- test.c
+++ test.c
@@ -15,49 +15,47 @@
 int main(int argc, char *arg[]){
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t sin_len=sizeof(client_addr);
-	int server_fd,client_fd;
+	int server,client;
 	int on=1;
 	int size;
   	char buf[BUF_SIZE];
   	char *method;
   	char *filename;
   	int status;
-  	int fdimg;
+  	int resource;
 
-	server_fd=socket(AF_INET,SOCK_STREAM,0);
+	server=socket(AF_INET,SOCK_STREAM,0);
 
-	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int));
+	setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(int));
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(8080);
 
-	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
+	if (bind(server, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
 	{
-		perror("bind");
-		close(server_fd);
+		printf("bind");
+		close(server);
 		exit(1);
 	}
 
-	if (listen(server_fd, 10) == -1)
+	if (listen(server, 10) == -1)
 	{
-		perror("listen");
-		close(server_fd);
+		printf("listen");
+		close(server);
 		exit(1);
 	}
 
 	while(1){
-		client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &sin_len);
-		if (client_fd < 0)
+		client = accept(server, (struct sockaddr *) &client_addr, &sin_len);
+		if (client < 0)
 		{
-			perror("Connection failed..\n");
+			printf("Not Connected\n");
 			continue;
 		}
-		printf("connection establised.\n");
-		recv(client_fd, buf, BUF_SIZE, 0);
+		printf("Connected.\n");
+		recv(client, buf, BUF_SIZE, 0);
 		method = strtok(buf, " ");
-		if (strcmp(method, "GET") != 0) 
-			return;
 		filename = strtok(NULL, " ");
 		
 		if (filename[0] == '/') filename++;
@@ -66,21 +64,24 @@ int main(int argc, char *arg[]){
 			status=1;
 		else
 			status=0;
-		if (status == 0) {
-    		printf("HTTP/1.0 200 OK\r\n\r\n");
-    		fdimg = open(filename, O_RDONLY);
-				sendfile(client_fd, fdimg, NULL, 6000);
-				close(fdimg);
-  		} else {
-    		printf("HTTP/1.0 404 Not Found\r\n\r\n");
-  			}
-  			FILE *file = fopen(filename, "r");
-  while(fgets(buf, BUF_SIZE, file)) {
-    send(client_fd, buf, strlen(buf), 0);
-    memset(buf, 0, BUF_SIZE);
 
-	}
-	close(client_fd);
+		if (status == 0) {
+    		printf("HTTP/1.1 200 OK\r\n\r\n");
+    		resource = open(filename, O_RDONLY);
+			sendfile(client, resource, NULL, 6000);
+			close(resource);
+
+			FILE *file = fopen(filename, "r");
+  			while(fgets(buf, BUF_SIZE, file)) {
+    		send(client, buf, strlen(buf), 0);
+    		memset(buf, 0, BUF_SIZE);
+		}
+  		} else {
+    		printf("HTTP/1.1 404 Not Found\r\n\r\n");
+  			}
+
+  			
+	close(client);
 }
 
 	return 0;
